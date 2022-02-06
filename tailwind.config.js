2c9c29871092350cbbb3b1fdb4d0fa5fba29d2fc
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-02-06 14:55:51 +0700

    Modify Tailwind color palettes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,23 @@
 module.exports = {
 	content: [ "./src/**/*.{js,jsx,ts,tsx}" ],
 	theme: {
-		extend: {
-			animation: {
-				"spin-slow": "spin 20s linear infinite"
+		colors: {
+			black: "#000000",
+			white: "#ffffff",
+			light: {
+				20: "#edfafd",
+				40: "#90e0ef",
+				60: "#00b4d8",
+				80: "#0077b6",
+				100: "#03045e"
 			},
-			colors: {
-				"react": {
-					"support": "#282c34",
-					"link": "#61dafb"
-				}
+			dark: {
+				0: "#10202d",
+				20: "#162b3c",
+				40: "#203e58",
+				60: "#315a7c",
+				80: "#8faabd",
+				100: "#bed2dd"
 			}
 		}
 	},
