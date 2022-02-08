dae8a09a9ccca0e7e351c8405721f187a8c8fcc4
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-02-08 22:58:10 +0700

    Increase sidebar gap between two main divs

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -16,7 +16,7 @@ function Sidebar({ active }: { active: string }) {
 
 	return (
 		<div className="w-32 h-screen py-16 bg-light-20">
-			<div className="flex flex-col justify-between items-center h-full mx-auto">
+			<div className="flex flex-col justify-between items-center gap-y-12 h-full mx-auto">
 				<div className="flex flex-col gap-y-16">
 					<div className="relative">
 						<Link to="/">
