0076aa9cab6fb87397a2399f691220ff329424d2
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-02-08 21:26:22 +0700

    Prevent stats card text wrapping

--- src/components/shared/StatsCard.tsx
+++ src/components/shared/StatsCard.tsx
@@ -3,7 +3,7 @@ import _ from "lodash";
 
 function StatsCard({ title, data, subtitle }: { title: string, data: string, subtitle?: string }) {
 	return (
-		<div className="w-44 px-6 py-4 space-y-0.5 bg-light-20 rounded-lg">
+		<div className="px-6 py-4 space-y-0.5 bg-light-20 rounded-lg">
 			<h3 className="font-medium text-light-80">{ title }</h3>
 			<div className="text-2xl font-semibold text-light-100">{ data }</div>
 			{
