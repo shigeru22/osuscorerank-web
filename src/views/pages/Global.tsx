64d09be854d155238f340a5bcbefd00aa3e18d26
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-02-09 23:20:18 +0700

    Add global page responsive view

--- src/views/pages/Global.tsx
+++ src/views/pages/Global.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import _ from "lodash";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import StatsCard from "../../components/shared/StatsCard";
 import RankingList from "../../components/shared/RankingList";
@@ -6,12 +7,16 @@ import Pagination from "../../components/shared/Pagination";
 import TextInput from "../../components/shared/inputs/Text";
 import Dropdown from "../../components/shared/inputs/Dropdown";
 import { getRankingListTotalPages } from "../../utils/Number";
+import { getTableRowsFromViewport } from "../../utils/RankingList";
 import { IRankingListData } from "../../types/components/RankingList";
 import { IDropdownData } from "../../types/components/Dropdown";
 
 function Global() {
 	const [ searchQuery, setSearchQuery ] = useState("");
 	const [ selectedSortId, setSelectedSortId ] = useState(1);
+	const [ tableRowsPerPage, setTableRowsPerPage ] = useState(getTableRowsFromViewport());
+
+	const [ updateDebounce, setUpdateDebounce ] = useState<ReturnType<typeof setTimeout> | undefined>(undefined);
 
 	const [ rankingPage, setRankingPage ] = useState(1);
 	const [ rankingData, setRankingData ] = useState<IRankingListData[]>([]);
@@ -27,13 +32,39 @@ function Global() {
 	];
 
 	useEffect(() => {
+		const updateWindowDimensions = () => {
+			if(!_.isUndefined(updateDebounce)) {
+				clearTimeout(updateDebounce);
+			}
+
+			setUpdateDebounce(setTimeout(() => {
+				setTableRowsPerPage(getTableRowsFromViewport());
+			}, 200));
+		};
+
+		window.addEventListener("resize", updateWindowDimensions);
+
+		return () => {
+			window.removeEventListener("resize", updateWindowDimensions);
+		};
+	}, [ updateDebounce ]);
+
+	useEffect(() => {
 		const temp: IRankingListData[] = [];
-		for(let i = (rankingPage - 1) * 5; i < rankingPage * 5; i++) {
+		for(let i = (rankingPage - 1) * tableRowsPerPage; i < rankingPage * tableRowsPerPage; i++) {
 			temp.push(rankingData[i]);
 		}
 
 		setDisplayedRankingData(temp);
-	}, [ rankingPage, rankingData ]);
+
+		if(!_.isUndefined(updateDebounce)) {
+			clearTimeout(updateDebounce);
+			setUpdateDebounce(undefined);
+		}
+
+	/* updateDebounce should not be its dependency */
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [ rankingPage, rankingData, tableRowsPerPage ]);
 
 	useEffect(() => {
 		const data: IRankingListData[] = [
@@ -105,17 +136,26 @@ function Global() {
 				<h1 className="font-semibold text-3xl text-light-100">Global</h1>
 				<h2 className="font-semibold text-light-60">Last updated: 2022/01/27</h2>
 			</div>
-			<h3 className="font-semibold text-2xl text-light-100">Statistics</h3>
-			<div className="flex items-start gap-x-4">
-				<StatsCard title="Recently Inactive" data="16" subtitle="+ 1 since last month" />
-				<StatsCard title="Total Inactives" data="143" subtitle="+ 5 since last month" />
-			</div>
-			<div className="flex items-start gap-x-6">
-				<div className="flex flex-col justify-between items-center h-64">
-					<RankingList data={ displayedRankingData } />
-					<Pagination active={ rankingPage } total={ getRankingListTotalPages(rankingData, 5) } setValue={ setRankingPage } />
+			<div className="2xl:flex 2xl:justify-between 2xl:gap-x-6 gap-y-6 space-y-6 2xl:space-y-0">
+				<div className="flex flex-col gap-y-6">
+					<h3 className="font-semibold text-2xl text-light-100">Statistics</h3>
+					<div className="flex 2xl:flex-col items-start gap-x-4 gap-y-4">
+						<StatsCard title="Recently Inactive" data="16" subtitle="+ 1 since last month" />
+						<StatsCard title="Total Inactives" data="143" subtitle="+ 5 since last month" />
+					</div>
 				</div>
-				<div className="flex flex-col gap-y-4 pt-1.25">
+				<div className="flex items-start gap-x-6">
+					<div className="flex flex-col items-center gap-y-4">
+						<h3 className="hidden 2xl:block self-start text-left font-semibold text-2xl text-light-100">Rankings</h3>
+						<RankingList data={ displayedRankingData } />
+						<Pagination active={ rankingPage } total={ getRankingListTotalPages(rankingData, 5) } setValue={ setRankingPage } />
+					</div>
+					<div className="flex 2xl:hidden flex-col gap-y-4 pt-1.25">
+						<TextInput name="search" label="Search player" icon={ faSearch } value={ searchQuery } setValue={ setSearchQuery } />
+						<Dropdown name="sort" label="Sort" data={ sortOptions } value={ selectedSortId } setValue={ setSelectedSortId } />
+					</div>
+				</div>
+				<div className="hidden 2xl:flex flex-col gap-y-4 pt-13">
 					<TextInput name="search" label="Search player" icon={ faSearch } value={ searchQuery } setValue={ setSearchQuery } />
 					<Dropdown name="sort" label="Sort" data={ sortOptions } value={ selectedSortId } setValue={ setSelectedSortId } />
 				</div>
