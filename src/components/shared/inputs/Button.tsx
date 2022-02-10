84ea8d696e7186a63648c867b8fca479eec8eba7
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-02-10 21:30:06 +0700

    Implement button dark mode

--- src/components/shared/inputs/Button.tsx
+++ src/components/shared/inputs/Button.tsx
@@ -3,7 +3,7 @@ import _ from "lodash";
 
 function Button({ label, type, onClick }: { label: string, type?: "primary" | "danger", onClick?: React.MouseEventHandler<HTMLButtonElement> }) {
 	return (
-		<button type="button" onClick={ onClick } className={ `w-48 py-1.5 font-medium ${ (!_.isUndefined(type) && type === "danger") ? "bg-danger-light active:bg-danger-light-active text-white" : "bg-light-20 active:bg-light-40" } rounded-lg` }>
+		<button type="button" onClick={ onClick } className={ `w-48 py-1.5 font-medium ${ (!_.isUndefined(type) && type === "danger") ? "bg-danger-light active:bg-danger-light-active dark:bg-danger-dark dark:active:bg-danger-dark-active text-white" : "bg-light-20 dark:bg-dark-0 active:bg-light-40 dark:active:bg-dark-40" } rounded-lg` }>
 			{ label }
 		</button>
 	);
