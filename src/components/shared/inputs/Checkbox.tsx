4d5a2e2e62714f9f07e850cd08bbabc913a7f123
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-02-10 21:26:26 +0700

    Implement checkbox dark mode

--- src/components/shared/inputs/Checkbox.tsx
+++ src/components/shared/inputs/Checkbox.tsx
@@ -7,8 +7,8 @@ function Checkbox({ name, label, value, setValue }: { name: string, label: strin
 
 	return (
 		<div className="flex items-center gap-x-2">
-			<input id={ name } type="checkbox" name={ name } checked={ value } onChange={ () => handleValueChange(!value) } className="form-checkbox w-5 h-5 border-2 focus:ring-0 border-light-60 focus:border-light-80 text-light-60 active:focus:text-light-80 focus:ring-white rounded" />
-			<label htmlFor={ name } className="font-medium">{ label }</label>
+			<input id={ name } type="checkbox" name={ name } checked={ value } onChange={ () => handleValueChange(!value) } className="form-checkbox w-5 h-5 border-2 focus:ring-0 active:ring-0 dark:ring-0 dark:active:ring-0 outline-none dark:outline-0 border-light-60 dark:border-dark-60 active:border-light-80 dark:focus:border-dark-80 dark:active:border-dark-60 dark:bg-dark-20 dark:active:bg-dark-60 text-light-60 active:focus:text-light-80 focus:ring-white dark:focus:ring-dark-20 rounded" />
+			<label htmlFor={ name } className="font-medium text-light-100 dark:text-dark-100">{ label }</label>
 		</div>
 	);
 }
