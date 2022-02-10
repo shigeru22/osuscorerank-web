d11609eaf977e025618d091772de61c349010fc2
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-02-10 21:32:47 +0700

    Implement stats card dark mode

--- src/components/shared/StatsCard.tsx
+++ src/components/shared/StatsCard.tsx
@@ -3,12 +3,12 @@ import _ from "lodash";
 
 function StatsCard({ title, data, subtitle }: { title: string, data: string, subtitle?: string }) {
 	return (
-		<div className="px-6 py-4 space-y-0.5 bg-light-20 rounded-lg">
-			<h3 className="font-medium text-light-80">{ title }</h3>
-			<div className="text-2xl font-semibold text-light-100">{ data }</div>
+		<div className="px-6 py-4 space-y-0.5 bg-light-20 dark:bg-dark-0 rounded-lg">
+			<h3 className="font-medium text-light-80 dark:text-dark-80">{ title }</h3>
+			<div className="text-2xl font-semibold text-light-100 dark:text-dark-100">{ data }</div>
 			{
 				!_.isUndefined(subtitle) &&
-				<p className="text-xs font-semibold text-light-80">{ subtitle }</p>
+				<p className="text-xs font-semibold text-light-80 dark:text-dark-60">{ subtitle }</p>
 			}
 		</div>
 	);
