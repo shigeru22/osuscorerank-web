e8ed0b23adc07246565cfd6fa6d8f3d76beab401
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-02-10 21:00:32 +0700

    Implement text input dark mode

--- src/components/shared/inputs/Text.tsx
+++ src/components/shared/inputs/Text.tsx
@@ -11,20 +11,20 @@ function TextInput({ name, label, icon, value, setValue }: { name: string, label
 	}
 
 	return (
-		<div className="w-56 space-y-2 font-medium text-light-80">
-			<label htmlFor={ name }>{ label }</label>
+		<div className="w-56 space-y-2">
+			<label htmlFor={ name } className="font-medium text-light-80 dark:text-dark-80">{ label }</label>
 			{
 				_.isUndefined(icon)
-					? <input type="text" id={ name } name={ name } value={ value } onChange={ e => handleTextChange(e.target.value) } className="form-input w-full px-3 py-1.5 font-medium bg-light-20 text-light-100 caret-light-100 border-none focus:ring-0 rounded-md" />
+					? <input type="text" id={ name } name={ name } value={ value } onChange={ e => handleTextChange(e.target.value) } className="form-input w-full px-3 py-1.5 font-medium bg-light-20 dark:bg-dark-40 text-light-100 caret-light-100 border-none focus:ring-0 rounded-md" />
 					:
 					<div className="flex group">
 						<label htmlFor={ name }>
-							<div className="py-1.5 pl-3 bg-light-20 rounded-l-md">
-								<FontAwesomeIcon icon={ icon } className={ `my-auto ${ !_.isEmpty(value) ? "text-light-80" : "text-light-40 group-hover:text-light-80" }` } />
+							<div className="py-1.5 pl-3 bg-light-20 dark:bg-dark-40 rounded-l-md">
+								<FontAwesomeIcon icon={ icon } className={ `my-auto ${ !_.isEmpty(value) ? "text-light-80 dark:text-dark-80" : "text-light-40 dark:text-dark-60 group-hover:text-light-80 dark:group-hover:text-dark-80" }` } />
 							</div>
 						</label>
 						<div className="flex-grow">
-							<input type="text" id={ name } name={ name } value={ value } onChange={ e => handleTextChange(e.target.value) } className="form-input w-full py-1.5 pl-2 pr-3 font-medium bg-light-20 text-light-100 caret-light-100 border-none focus:ring-0 rounded-r-md" />
+							<input type="text" id={ name } name={ name } value={ value } onChange={ e => handleTextChange(e.target.value) } className="form-input w-full py-1.5 pl-2 pr-3 font-medium bg-light-20 dark:bg-dark-40	 text-light-100 dark:text-dark-100 caret-light-100 dark:caret-dark-100 border-none focus:ring-0 rounded-r-md" />
 						</div>
 					</div>
 			}
