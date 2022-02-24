2e8f223520d4167e8745b09e6b01812957b4881f
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-02-24 11:19:39 +0700

    Fix dropdown menus on lg screens

--- src/components/shared/inputs/Dropdown.tsx
+++ src/components/shared/inputs/Dropdown.tsx
@@ -93,7 +93,7 @@ function Dropdown({ name, label, data, value, setValue }: { name: string, label:
 			<div>
 				<label htmlFor={ name } className="font-medium text-light-80 dark:text-dark-80 whitespace-nowrap">{ label }</label>
 			</div>
-			<div className="flex justify-end w-full">
+			<div className="flex flex-col justify-end">
 				<button type="button" id={ name } onClick={ () => toggleDropdown() } ref={ refButton } className={ `flex flex-row justify-between items-center group max-w-full md:w-full px-3 py-1.5 ${ isOpened ? "bg-light-40 dark:bg-dark-60" : "bg-light-20 dark:bg-dark-40" } rounded-lg` }>
 					<div className="md:min-w-24 font-medium text-left text-light-100 dark:text-dark-100 whitespace-nowrap overflow-ellipsis">{ getValueFromData(value) }</div>
 					<FontAwesomeIcon icon={ faChevronDown } className={ `text-2xl pl-3 ${ isOpened ? "text-light-80 dark:text-dark-100" : "text-light-60 dark:text-dark-80 group-hover:text-light-80 dark:group-hover:text-dark-100" }` } />
@@ -101,7 +101,7 @@ function Dropdown({ name, label, data, value, setValue }: { name: string, label:
 				{
 					isOpened &&
 					<>
-						<div className="md:hidden">
+						<div className="lg:hidden">
 							<DimBackground>
 								<Dialog htmlRef={ refMobileDropdown } title={ label } onCancelClick={ () => setOpened(false) }>
 									{
@@ -112,16 +112,14 @@ function Dropdown({ name, label, data, value, setValue }: { name: string, label:
 								</Dialog>
 							</DimBackground>
 						</div>
-						<div className="hidden md:block">
-							<div className="relative flex justify-center z-10">
-								<div className="absolute flex flex-col w-full m-2 p-3 bg-light-20 dark:bg-dark-0 rounded-lg">
-									<div ref={ refDropdown } className="max-h-26 space-y-1 overflow-y-auto">
-										{
-											data.map(item => (
-												<div key={ item.id } onClick={ () => handleValueChange(item.id) } className={ `px-2 py-1 font-medium ${ item.id === value ? "bg-light-60 dark:bg-dark-40 text-white dark:text-dark-100" : "hover:bg-light-40 dark:hover:bg-dark-20 text-light-100 dark:text-dark-100" } rounded-lg cursor-pointer` }>{ item.name }</div>
-											))
-										}
-									</div>
+						<div className="relative hidden lg:flex justify-center z-10">
+							<div className="absolute flex flex-col w-full m-2 p-3 bg-light-20 dark:bg-dark-0 rounded-lg">
+								<div ref={ refDropdown } className="max-h-26 space-y-1 overflow-y-auto">
+									{
+										data.map(item => (
+											<div key={ item.id } onClick={ () => handleValueChange(item.id) } className={ `px-2 py-1 font-medium ${ item.id === value ? "bg-light-60 dark:bg-dark-40 text-white dark:text-dark-100" : "hover:bg-light-40 dark:hover:bg-dark-20 text-light-100 dark:text-dark-100" } rounded-lg cursor-pointer` }>{ item.name }</div>
+										))
+									}
 								</div>
 							</div>
 						</div>
