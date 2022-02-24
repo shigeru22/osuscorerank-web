f338e0f9f05255bba782e957f683d4253dcadf4d
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-02-24 11:51:47 +0700

    Accept multiple element children

--- src/components/shared/DimBackground.tsx
+++ src/components/shared/DimBackground.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function DimBackground({ children }: { children?: JSX.Element }) {
+function DimBackground({ children }: { children?: JSX.Element | JSX.Element[] }) {
 	return (
 		<div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-40 dark:bg-opacity-80 z-10">
 			<div className="relative top-0 left-0 w-full h-full">
