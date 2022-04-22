dbc22b0c4d6d1778fb6b375cdea7de898dabbfb6
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-04-22 22:11:42 +0700

    Implement profile dialog rankings

--- src/components/shared/ProfileDialog.tsx
+++ src/components/shared/ProfileDialog.tsx
@@ -6,11 +6,13 @@ import Button from "./inputs/Button";
 import { faTimes, faCircleNotch, faStar } from "@fortawesome/free-solid-svg-icons";
 import { settingsContext } from "../../views/App";
 import { numberToSeparatedThousandsString } from "../../utils/Number";
-import { getUserScore } from "../../utils/api/Scores";
+import { getCountryScores, getGlobalScores, getUserScore } from "../../utils/api/Scores";
 import { LogType } from "../../utils/Logging";
+import { IResponseData } from "../../types/api/Response";
+import { ICountryScoreResponseData, IGlobalScoreResponseData } from "../../types/api/Score";
 
 function ProfileDialog({ htmlRef, userId, starred, setOpened, onCloseClick, onStarClick }: { htmlRef?: React.Ref<HTMLDivElement>, userId: number, starred: boolean, setOpened: React.Dispatch<React.SetStateAction<boolean>>, onCloseClick: () => void, onStarClick: () => void }) {
-	const { settings, addLogData, setShowErrorDialog } = useContext(settingsContext);
+	const { addLogData, setShowErrorDialog } = useContext(settingsContext);
 
 	const [ isFetched, setFetched ] = useState(false); // height = 420 - 48px for fetching
 	const [ isLoading, setLoading ] = useState(true);
@@ -33,19 +35,42 @@ function ProfileDialog({ htmlRef, userId, starred, setOpened, onCloseClick, onSt
 			const userScore = await getUserScore(userId); // TODO: use get user score endpoint
 
 			if(!_.isUndefined(userScore.data)) {
+				let rankings: (IResponseData<ICountryScoreResponseData> | IResponseData<IGlobalScoreResponseData>)[] = [];
+				{
+					const requests = [];
+					requests.push(getCountryScores(userScore.data.score.user.country.countryId, 1, false));
+					requests.push(getCountryScores(userScore.data.score.user.country.countryId, 2, false));
+					requests.push(getGlobalScores(1, false));
+					requests.push(getGlobalScores(2, false));
+
+					rankings = await Promise.all(requests);
+				}
+
 				setUserName(userScore.data.score.user.userName);
 				setOsuId(userScore.data.score.user.osuId);
 				setCountryName(userScore.data.score.user.country.countryName);
 				setCountryCode(userScore.data.score.user.country.countryCode);
 
-				setScore(0);
-				setPerformancePoints(0);
+				setScore(_.isString(userScore.data.score.score) ? _.parseInt(userScore.data.score.score, 10) : userScore.data.score.score);
+				setPerformancePoints(userScore.data.score.user.isActive ? userScore.data.score.pp : -1);
 
-				/* TODO: move to WebAssembly? */
-				setCountryScoreRank(1);
-				setCountryPerformanceRank(1);
-				setGlobalScoreRank(1);
-				setGlobalPerformanceRank(1);
+				/* [0]: country score, [1]: country pp, [2]: global score, [3]: global pp */
+
+				if(!_.isUndefined(rankings[0].data)) {
+					setCountryScoreRank(rankings[0].data.scores.findIndex(score => score.user.userId === userId) + 1);
+				}
+
+				if(!_.isUndefined(rankings[1].data)) {
+					setCountryPerformanceRank(rankings[1].data.scores.findIndex(score => score.user.userId === userId) + 1);
+				}
+
+				if(!_.isUndefined(rankings[2].data)) {
+					setGlobalScoreRank(rankings[2].data.scores.findIndex(score => score.user.userId === userId) + 1);
+				}
+
+				if(!_.isUndefined(rankings[3].data)) {
+					setGlobalPerformanceRank(rankings[3].data.scores.findIndex(score => score.user.userId === userId) + 1);
+				}
 
 				setLoading(false);
 				setFetched(true);
@@ -75,6 +100,7 @@ function ProfileDialog({ htmlRef, userId, starred, setOpened, onCloseClick, onSt
 
 		setShowErrorDialog(true);
 	}
+
 	function handleOpenOsuProfile() {
 		window.open(`https://osu.ppy.sh/users/${ osuId }`, "blank");
 	}
@@ -90,47 +116,37 @@ function ProfileDialog({ htmlRef, userId, starred, setOpened, onCloseClick, onSt
 				<div className="w-full p-6 lg:px-12 lg:py-10 space-y-4">
 					{
 						isFetched ?
-							<>
-								{
-									(settings.osuClient.clientId === -1 || settings.osuClient.clientSecret === "") &&
-									<div className="w-full px-2 py-1">
-										<h4 className="font-semibold text-center text-light-60 dark:text-dark-60">
-											For current score and pp count, add your client credentials for accessing osu! API.
-										</h4>
-									</div>
-								}
-								<div className="flex flex-col lg:flex-row items-center gap-4">
-									<div className="flex flex-col items-center gap-y-2">
-										<div className="w-28 bg-light-40 dark:bg-dark-40 rounded-3xl">
-											<img src={ `https://a.ppy.sh/${ osuId }` } className="rounded-3xl" />
-											<div className="relative bottom-0 right-0">
-												<button type="button" onClick={ () => handleStarClick() } className="absolute bottom-2 right-2">
-													<FontAwesomeIcon icon={ faStar } className={ `text-xl ${ starred ? "text-light-80 dark:text-dark-80" : "text-white hover:text-light-40 dark:text-dark-20 dark:hover:text-dark-40 stroke-10 stroke-light-80 dark:stroke-dark-80" }` } />
-												</button>
-											</div>
+							<div className="flex flex-col lg:flex-row items-center gap-4">
+								<div className="flex flex-col items-center gap-y-2">
+									<div className="w-28 bg-light-40 dark:bg-dark-40 rounded-3xl">
+										<img src={ `https://a.ppy.sh/${ osuId }` } className="rounded-3xl" />
+										<div className="relative bottom-0 right-0">
+											<button type="button" onClick={ () => handleStarClick() } className="absolute bottom-2 right-2">
+												<FontAwesomeIcon icon={ faStar } className={ `text-xl ${ starred ? "text-light-80 dark:text-dark-80" : "text-white hover:text-light-40 dark:text-dark-20 dark:hover:text-dark-40 stroke-10 stroke-light-80 dark:stroke-dark-80" }` } />
+											</button>
 										</div>
-										<div className="flex items-center gap-x-2">
-											<ReactCountryFlag countryCode={ countryCode } svg alt={ countryName } title={ countryName } className="text-lg rounded-md" />
-											<div className="font-semibold text-lg text-light-100 dark:text-dark-100 overflow-ellipsis">{ userName }</div>
-										</div>
-										<Button label="osu! profile" onClick={ () => handleOpenOsuProfile() } />
 									</div>
-									<div className="md:flex-grow grid grid-cols-3 gap-x-2 gap-y-1">
-										<div className="font-semibold text-light-100 dark:text-dark-100">Score</div>
-										<div className="col-span-2 font-semibold text-right text-light-80 dark:text-dark-80">{ numberToSeparatedThousandsString(score) }</div>
-										<div className="font-medium text-light-60 dark:text-dark-60">Country</div>
-										<div className="col-span-2 font-medium text-right text-light-80 dark:text-dark-80"># { numberToSeparatedThousandsString(countryScoreRank) }</div>
-										<div className="font-medium text-light-60 dark:text-dark-60">Global</div>
-										<div className="col-span-2 font-medium text-right text-light-80 dark:text-dark-80"># { numberToSeparatedThousandsString(globalScoreRank) }</div>
-										<div className="font-semibold text-light-100 dark:text-dark-100">pp</div>
-										<div className="col-span-2 font-semibold text-right text-light-80 dark:text-dark-80">{ numberToSeparatedThousandsString(performancePoints) }</div>
-										<div className="font-medium text-light-60 dark:text-dark-60">Country</div>
-										<div className="col-span-2 font-medium text-right text-light-80 dark:text-dark-80"># { numberToSeparatedThousandsString(countryPerformanceRank) }</div>
-										<div className="font-medium text-light-60 dark:text-dark-60">Global</div>
-										<div className="col-span-2 font-medium text-right text-light-80 dark:text-dark-80"># { numberToSeparatedThousandsString(globalPerformanceRank) }</div>
+									<div className="flex items-center gap-x-2">
+										<ReactCountryFlag countryCode={ countryCode } svg alt={ countryName } title={ countryName } className="text-lg rounded-md" />
+										<div className="font-semibold text-lg text-light-100 dark:text-dark-100 overflow-ellipsis">{ userName }</div>
 									</div>
+									<Button label="osu! profile" onClick={ () => handleOpenOsuProfile() } />
 								</div>
-							</>
+								<div className="md:flex-grow grid grid-cols-3 gap-x-2 gap-y-1">
+									<div className="font-semibold text-light-100 dark:text-dark-100">Score</div>
+									<div className="col-span-2 font-semibold text-right text-light-80 dark:text-dark-80">{ numberToSeparatedThousandsString(score) }</div>
+									<div className="font-medium text-light-60 dark:text-dark-60">Country</div>
+									<div className="col-span-2 font-medium text-right text-light-80 dark:text-dark-80"># { numberToSeparatedThousandsString(countryScoreRank) }</div>
+									<div className="font-medium text-light-60 dark:text-dark-60">Global</div>
+									<div className="col-span-2 font-medium text-right text-light-80 dark:text-dark-80"># { numberToSeparatedThousandsString(globalScoreRank) }</div>
+									<div className="font-semibold text-light-100 dark:text-dark-100">pp</div>
+									<div className="col-span-2 font-semibold text-right text-light-80 dark:text-dark-80">{ performancePoints > -1 ? numberToSeparatedThousandsString(performancePoints) : "-" }</div>
+									<div className="font-medium text-light-60 dark:text-dark-60">Country</div>
+									<div className="col-span-2 font-medium text-right text-light-80 dark:text-dark-80"># { numberToSeparatedThousandsString(countryPerformanceRank) }</div>
+									<div className="font-medium text-light-60 dark:text-dark-60">Global</div>
+									<div className="col-span-2 font-medium text-right text-light-80 dark:text-dark-80"># { numberToSeparatedThousandsString(globalPerformanceRank) }</div>
+								</div>
+							</div>
 							:
 							<div className="flex justify-center items-center w-full h-93 lg:h-48">
 								<div className="flex flex-col justify-center items-center gap-y-2">
