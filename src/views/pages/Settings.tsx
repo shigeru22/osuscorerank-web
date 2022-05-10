c75e3b763ab209218b16e79d73caa0e74dd22db2
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-05-10 13:45:54 +0700

    Disable scrolling when reset dialog is shown

--- src/views/pages/Settings.tsx
+++ src/views/pages/Settings.tsx
@@ -74,6 +74,16 @@ function Settings() {
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [ themeId, dateFormatId, defaultCountryId, defaultSortingId, settings.starredUserId, setSettings ]);
 
+	useEffect(() => {
+		if(showResetUsersDialog) {
+			document.body.classList.add("overflow-y-hidden");
+		}
+		else {
+			document.body.classList.remove("overflow-y-hidden");
+			document.body.classList.length <= 0 && document.body.removeAttribute("class");
+		}
+	}, [ showResetUsersDialog ]);
+
 	async function getWasmGreetMessage() {
 		setWasmMessage("Testing...");
 		setWasmStatus("Testing...");
@@ -183,73 +193,75 @@ function Settings() {
 	}
 
 	return (
-		<div className="px-8 py-0 md:px-14 md:py-8 lg:py-12 md:space-y-6">
-			<h1 className="hidden md:inline font-semibold text-3xl text-light-100 dark:text-dark-100">Settings</h1>
-			<div className="flex flex-col xl:flex-row gap-x-12 gap-y-4 pb-6">
-				<div className="xl:basis-1/2 md:w-1/2 lg:w-auto space-y-6">
-					<div className="pt-2 md:pt-0 space-y-4">
-						<h3 className="font-semibold text-2xl text-light-100 dark:text-dark-100">General</h3>
-						<div className="flex flex-col lg:flex-row gap-x-12 gap-y-2">
-							<Dropdown name="theme" label="Theme" data={ themeOptions } value={ themeId } setValue={ setThemeId } />
-							<Dropdown name="dateformat" label="Date format" data={ dateFormatOptions } value={ dateFormatId } setValue={ setDateFormatId } />
+		<>
+			<div className="px-8 py-0 md:px-14 md:py-8 lg:py-12 md:space-y-6">
+				<h1 className="hidden md:inline font-semibold text-3xl text-light-100 dark:text-dark-100">Settings</h1>
+				<div className="flex flex-col xl:flex-row gap-x-12 gap-y-4 pb-6">
+					<div className="xl:basis-1/2 md:w-1/2 lg:w-auto space-y-6">
+						<div className="pt-2 md:pt-0 space-y-4">
+							<h3 className="font-semibold text-2xl text-light-100 dark:text-dark-100">General</h3>
+							<div className="flex flex-col lg:flex-row gap-x-12 gap-y-2">
+								<Dropdown name="theme" label="Theme" data={ themeOptions } value={ themeId } setValue={ setThemeId } />
+								<Dropdown name="dateformat" label="Date format" data={ dateFormatOptions } value={ dateFormatId } setValue={ setDateFormatId } />
+							</div>
+						</div>
+						<div className="space-y-4">
+							<h3 className="font-semibold text-2xl text-light-100 dark:text-dark-100">Defaults</h3>
+							<div className="flex flex-col lg:flex-row gap-x-12 gap-y-2">
+								<Dropdown name="defaultcountry" label="Country" data={ countries } value={ defaultCountryId } setValue={ setDefaultCountryId } />
+								<Dropdown name="defaultsorting" label="Sorting" data={ sortOptions } value={ defaultSortingId } setValue={ setDefaultSortingId } />
+							</div>
+						</div>
+						<div className="space-y-4">
+							<h3 className="font-semibold text-2xl text-light-100 dark:text-dark-100">Starred Users</h3>
+							<div className="space-y-2">
+								<h6 className="font-medium text-light-80 dark:text-dark-80">Starred ranking list coming soon. Right now, you&apos;re able to star users at users&apos; profile dialog.</h6>
+								<h6 className="font-medium text-light-80 dark:text-dark-80">Total starred user: { starredUsers.length }</h6>
+								<Button type="danger" label="Reset all users" onClick={ () => setShowResetUsersDialog(true) } />
+							</div>
+						</div>
+						<div className="space-y-4">
+							<h3 className="font-semibold text-2xl text-light-100 dark:text-dark-100">Logging</h3>
+							<div className="space-y-2">
+								<h6 className="font-medium text-light-80 dark:text-dark-80">These logs are stored locally for debugging purposes and never leave your browser. All logs will be automatically deleted upon closing.</h6>
+								<h6 className="font-medium text-light-80 dark:text-dark-80">If any problems are found, feel free to submit feedbacks to GitHub issues along with these logs.</h6>
+								<Button label="Open Logs" onClick={ () => setShowErrorDialog(true) } />
+							</div>
+							<h6 className="font-medium text-light-40 dark:text-dark-60">osu-inactive-score { getUpdateVersionString(VersionType.WEB_VERSION) }</h6>
 						</div>
 					</div>
-					<div className="space-y-4">
-						<h3 className="font-semibold text-2xl text-light-100 dark:text-dark-100">Defaults</h3>
-						<div className="flex flex-col lg:flex-row gap-x-12 gap-y-2">
-							<Dropdown name="defaultcountry" label="Country" data={ countries } value={ defaultCountryId } setValue={ setDefaultCountryId } />
-							<Dropdown name="defaultsorting" label="Sorting" data={ sortOptions } value={ defaultSortingId } setValue={ setDefaultSortingId } />
+					<div className="xl:basis-1/2 md:w-1/2 lg:w-auto space-y-6">
+						<div className="space-y-4">
+							<h3 className="font-semibold text-2xl text-light-100 dark:text-dark-100">API</h3>
+							<div className="space-y-2">
+								<h6 className="font-medium text-light-80 dark:text-dark-80">Details on external API access will be added soon.</h6>
+								<h6 className="font-medium text-light-80 dark:text-dark-80">API Status: { getApiStatusString() }</h6>
+								<Button type="primary" label="Check Status" onClick={ () => handleApiTest() } />
+							</div>
+							<h6 className="font-medium text-light-40 dark:text-dark-60">osuinactive-api { getUpdateVersionString(VersionType.API_VERSION) }</h6>
 						</div>
-					</div>
-					<div className="space-y-4">
-						<h3 className="font-semibold text-2xl text-light-100 dark:text-dark-100">Starred Users</h3>
-						<div className="space-y-2">
-							<h6 className="font-medium text-light-80 dark:text-dark-80">Starred ranking list coming soon. Right now, you&apos;re able to star users at users&apos; profile dialog.</h6>
-							<h6 className="font-medium text-light-80 dark:text-dark-80">Total starred user: { starredUsers.length }</h6>
-							<Button type="danger" label="Reset all users" onClick={ () => setShowResetUsersDialog(true) } />
+						<div className="space-y-4">
+							<h3 className="font-semibold text-2xl text-light-100 dark:text-dark-100">WebAssembly</h3>
+							<div className="space-y-2">
+								<h6 className="font-medium text-light-80 dark:text-dark-80">This runs greet() function and does a simple search test using compiled Wasm module.</h6>
+								<h6 className="font-medium text-light-80 dark:text-dark-80">Message: { wasmMessage }</h6>
+								<h6 className="font-medium text-light-80 dark:text-dark-80">Search test: { wasmStatus }</h6>
+								<Button type="primary" label="Test Again" onClick={ () => getWasmGreetMessage() } />
+							</div>
+							<h6 className="font-medium text-light-40 dark:text-dark-60">osuinactivescore-wasm { getUpdateVersionString(VersionType.WEB_VERSION) }</h6>
 						</div>
-					</div>
-					<div className="space-y-4">
-						<h3 className="font-semibold text-2xl text-light-100 dark:text-dark-100">Logging</h3>
-						<div className="space-y-2">
-							<h6 className="font-medium text-light-80 dark:text-dark-80">These logs are stored locally for debugging purposes and never leave your browser. All logs will be automatically deleted upon closing.</h6>
-							<h6 className="font-medium text-light-80 dark:text-dark-80">If any problems are found, feel free to submit feedbacks to GitHub issues along with these logs.</h6>
-							<Button label="Open Logs" onClick={ () => setShowErrorDialog(true) } />
-						</div>
-						<h6 className="font-medium text-light-40 dark:text-dark-60">osu-inactive-score { getUpdateVersionString(VersionType.WEB_VERSION) }</h6>
-					</div>
-				</div>
-				<div className="xl:basis-1/2 md:w-1/2 lg:w-auto space-y-6">
-					<div className="space-y-4">
-						<h3 className="font-semibold text-2xl text-light-100 dark:text-dark-100">API</h3>
-						<div className="space-y-2">
-							<h6 className="font-medium text-light-80 dark:text-dark-80">Details on external API access will be added soon.</h6>
-							<h6 className="font-medium text-light-80 dark:text-dark-80">API Status: { getApiStatusString() }</h6>
-							<Button type="primary" label="Check Status" onClick={ () => handleApiTest() } />
-						</div>
-						<h6 className="font-medium text-light-40 dark:text-dark-60">osuinactive-api { getUpdateVersionString(VersionType.API_VERSION) }</h6>
-					</div>
-					<div className="space-y-4">
-						<h3 className="font-semibold text-2xl text-light-100 dark:text-dark-100">WebAssembly</h3>
-						<div className="space-y-2">
-							<h6 className="font-medium text-light-80 dark:text-dark-80">This runs greet() function and does a simple search test using compiled Wasm module.</h6>
-							<h6 className="font-medium text-light-80 dark:text-dark-80">Message: { wasmMessage }</h6>
-							<h6 className="font-medium text-light-80 dark:text-dark-80">Search test: { wasmStatus }</h6>
-							<Button type="primary" label="Test Again" onClick={ () => getWasmGreetMessage() } />
-						</div>
-						<h6 className="font-medium text-light-40 dark:text-dark-60">osuinactivescore-wasm { getUpdateVersionString(VersionType.WEB_VERSION) }</h6>
 					</div>
 				</div>
 			</div>
 			{
 				showResetUsersDialog &&
-					<DimBackground>
-						<Dialog htmlRef={ refResetStarredDialog } title="Reset All Users" onCancelClick={ () => setShowResetUsersDialog(false) } onOkayClick={ handleResetStarredUsers }>
-							<div className="font-medium text-light-80 dark:text-dark-80">Remove all starred users?</div>
-						</Dialog>
-					</DimBackground>
+				<DimBackground>
+					<Dialog htmlRef={ refResetStarredDialog } title="Reset All Users" onCancelClick={ () => setShowResetUsersDialog(false) } onOkayClick={ handleResetStarredUsers }>
+						<div className="font-medium text-light-80 dark:text-dark-80">Remove all starred users?</div>
+					</Dialog>
+				</DimBackground>
 			}
-		</div>
+		</>
 	);
 }
 
