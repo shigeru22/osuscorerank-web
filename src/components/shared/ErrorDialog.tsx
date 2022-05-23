17a8a0b6631ec63fbb13ebe979aefe096d8a8cfd
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2022-05-23 22:08:10 +0700

    Increase error dialog size for medium screens

--- src/components/shared/ErrorDialog.tsx
+++ src/components/shared/ErrorDialog.tsx
@@ -33,7 +33,7 @@ function ErrorDialog({ htmlRef, data, onCancelClick }: { htmlRef?: React.Ref<HTM
 
 	return (
 		<div className="flex justify-center items-center w-full h-full z-20">
-			<div ref={ htmlRef } className="flex flex-col gap-y-4 min-w-48 max-w-md w-5/6 m-2 p-6 bg-white dark:bg-dark-0 rounded-lg">
+			<div ref={ htmlRef } className="flex flex-col gap-y-4 min-w-48 md:min-w-56 max-w-md md:max-w-xl w-5/6 m-2 p-6 bg-white dark:bg-dark-0 rounded-lg">
 				<h3 className="font-semibold text-xl text-light-100 dark:text-dark-100">Error Details</h3>
 				<div className="max-h-80 space-y-2 overflow-y-auto">
 					<p className="font-medium text-light-80 dark:text-dark-80">
@@ -46,7 +46,7 @@ function ErrorDialog({ htmlRef, data, onCancelClick }: { htmlRef?: React.Ref<HTM
 						</button>
 						{
 							isExpanded &&
-							<div className="flex flex-col max-h-26 px-3 py-1.5 bg-light-20 dark:bg-dark-20 overflow-y-auto rounded-lg">
+							<div className="flex flex-col max-h-26 md:max-h-36 px-3 py-1.5 bg-light-20 dark:bg-dark-20 overflow-y-auto rounded-lg">
 								{
 									data.map(item => (
 										<LogElement key={ item.id } log={ item } />
